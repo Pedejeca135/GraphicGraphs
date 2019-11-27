3d6d33c34f36c19ae7d9e22d49e0f74adcce4772
Pedro Cantú <44269967+Pedejeca135@users.noreply.github.com>
2019-11-26 19:58:00 -0600

    i improve the graph making, the Line class now have two nodes to indicate what is comunicatin. Now you can eliminate the nodes and automaticly the edges that connect it are removed, i have to fix the load and save of graphs.

--- graphical_graph_editor/Form1.Designer.cs
+++ graphical_graph_editor/Form1.Designer.cs
@@ -67,6 +67,7 @@
             this.Controls.Add(this.toolStrip1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             this.toolStrip1.ResumeLayout(false);
